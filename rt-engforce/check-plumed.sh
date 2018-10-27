ca4196c539ff7a444460fad8908e51f718819de9
Gareth Tribello <gareth.tribello@gmail.com>
2018-10-27 18:23:47 +0100

    Added test on adding forces on energy

--- rt-engforce/check-plumed.sh
+++ rt-engforce/check-plumed.sh
@@ -7,5 +7,19 @@ LAMMPS=/Users/gareth/MD_code/lammps-permanent/lammps/src/lmp_mpi
 # Nothing from here works
 
 # Now run calculations to check forces on energy
-$LAMMPS < in.peptide-plumed-engforce-ref
-$LAMMPS < in.peptide-plumed-eng-force-plumed
+cd Sim1 
+$LAMMPS < start.lmp
+cd ../
+
+cd Sim2
+$LAMMPS < start.lmp
+cd ../
+
+nlines=`paste Sim1/energy1 Sim2/energy2 | tail -n +2 | awk '{if( $2<$4-0.0001 || $2>04+0.0001 ) print $0}' | wc -l`
+if [ "$nlines" -gt 0 ] ; then
+   echo ERROR mismatched energies for restrained NVT simuations
+   paste Sim1/energy1 Sim2/energy2 | awk '{if( $2<$4-0.0001 || $2>04+0.0001 ) print $0}'
+fi
+
+rm -f Sim1/bck* Sim1/energy1 Sim1/plumed.out Sim1/restart.lmp Sim1/restart2.lmp Sim1/log.lammps Sim1/data.final
+rm -f Sim2/bck* Sim2/energy2 Sim2/plumed.out Sim2/restart.lmp Sim2/restart2.lmp Sim2/log.lammps Sim2/data/final
