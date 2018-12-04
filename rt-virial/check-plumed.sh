e5c8bdad2d1c39efff3739fc5b80f9a8d167e348
Gareth Tribello <gareth.tribello@gmail.com>
2018-12-04 11:39:34 +0000

    Checking what is wrong with npt test of virial on travis

--- rt-virial/check-plumed.sh
+++ rt-virial/check-plumed.sh
@@ -11,6 +11,7 @@ $LAMMPS < in.peptide-plumed-npt2
 nlines=`paste plmd_volume_with_restraint plmd_volume_without_restraint | tail -n +2 | awk '{if( $2<$4-0.0001 || $2>$4+0.0001 ) print $0}' | wc -l`
 if [ "$nlines" -gt 0 ] ; then
       echo ERROR passing virial from PLUMED back to LAMMPS
+      paste plmd_volume_with_restraint plmd_volume_without_restraint | tail -n +2 | awk '{if( $2<$4-0.0001 || $2>$4+0.0001 ) print $0}'
 fi
 
 rm bck.* log.lammps p.log plmd_volume_without_restraint plmd_volume_with_restraint
